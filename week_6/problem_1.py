781d15b42b74a4282a21abb66c025785c2640eb4
gertyboy1200 <garrett.kolenbrander@gmail.com>
2025-02-10 14:24:56 -0700

    problem 1 🏁
    clean up some white space

--- week_6/problem_1.py
+++ week_6/problem_1.py
@@ -1,6 +1,5 @@
 import random
 
-
 def get_random_day(n):
     week_days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     print("Your random week day is:", week_days[n])
