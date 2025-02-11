4b7741c86e574263ed435a54638db6540c5202d4
gertyboy1200 <garrett.kolenbrander@gmail.com>
2025-02-11 12:39:35 -0700

    Adjust problem 2-14 🚧

--- week_3/problem_2-14.py
+++ week_3/problem_2-14.py
@@ -1,9 +1,12 @@
-with open('problem_2-14.txt') as file:
-    line = file.readline()
+word1 = "All"
+word2 = "work"
+word3 = "and"
+word4 = "no"
+word5 = "play"
+word6 = "makes"
+word7 = "Jack"
+word8 = "a"
+word9 = "dull"
+word10 = "boy."
 
-words = line.split()
-
-for word in words:
-    print(word)
-
-
+print(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10)
